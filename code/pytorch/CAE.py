Corrected calculation of single losses

code/pytorch/CAE.py
@@ -6,9 +6,12 @@ from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import metrics
-import pandas as pd 
+import pandas as pd
+import os
 
 from models.cae_pytorch import CAE_pytorch
+
+absolute_path = os.path.dirname(__file__)
 
 def prepare_mnist(selected_label, p):
     transform = transforms.ToTensor()
@@ -44,15 +47,16 @@ def prepare_mnist(selected_label, p):
     
     return train_loader, test_loader
 
-def train_cae_my(train_loader, model, criterion, optimizer, epochs, train_loss_percent):
-    outputs = []
-    losses = []
+def train_cae_my(train_loader, model, criterion, optimizer, epochs, train_loss_percent, device):
     for epoch in range(num_epochs):
         imgs = []
         for (img, _) in train_loader:
-            recon = model(img)
-            loss = criterion(recon, img)
-            imgs.append((img, loss))
+            for img in batch:
+                img = img.to(device)
+                img = img.unsqueeze(0)
+                recon = model(img)
+                loss = criterion(recon, img)
+                imgs.append((img, loss))
 
         imgs.sort(key=lambda x: x[1])
         l = int(train_loss_percent * len(imgs))
@@ -69,14 +73,15 @@ def train_cae_my(train_loader, model, criterion, optimizer, epochs, train_loss_p
             optimizer.step()
 
         print(f'Epoch:{epoch+1}, Loss:{loss.item():.4f}')
-        outputs.append((epoch, img, recon))
     
-    return outputs
+    return None
 
-def train_cae(train_loader, model, criterion, optimizer, epochs):
+def train_cae(train_loader, model, criterion, optimizer, epochs, device):
+    model = model.to(device)
     for epoch in range(num_epochs):
         for data in train_loader:
             img, _ = data
+            img = img.to(device)
             optimizer.zero_grad()
             output = model(img)
             loss = criterion(output, img)
@@ -85,18 +90,23 @@ def train_cae(train_loader, model, criterion, optimizer, epochs):
     return None
 
 def complete_run_cae(file_prefix, selected_label=9, p=0.05, train_loss_percent=0.5, num_epochs=30):
+    
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    #device = torch.device("cpu")
     train_loader, test_loader = prepare_mnist(selected_label, p)
     
     model = CAE_pytorch(in_channels=1)
+    model = model.to(device)
 
     criterion = nn.MSELoss()
     #optimizer = torch.optim.Adam(model.parameters(),lr=1e-4,weight_decay=1e-5)
     optimizer = torch.optim.Adam(model.parameters(),eps=1e-7, weight_decay=0.0005)
     
     outputs = None
-    #outputs = train_cae(train_loader, model, criterion, optimizer, epochs=30)
-    outputs = train_cae_my(train_loader, model, criterion, optimizer, epochs=num_epochs, train_loss_percent=train_loss_percent)
+    #outputs = train_cae(train_loader, model, criterion, optimizer, epochs=250, device=device)
+    outputs = train_cae_my(train_loader, model, criterion, optimizer, epochs=num_epochs, train_loss_percent=train_loss_percent, device=device)
 
+    """
     if outputs is not None:
         k = num_epochs-1
         plt.figure(figsize=(9, 2))
@@ -114,12 +124,14 @@ def complete_run_cae(file_prefix, selected_label=9, p=0.05, train_loss_percent=0
             plt.imshow(item[0])
         plt.savefig(file_prefix+"-lables.png")
         plt.close()
+    """
     
-    def calculate_losses(loader):
+    def calculate_losses(loader, device):
         normal = []
         anomalie = []
         anomalie_label = []
         for (img, label) in loader:
+            img = img.to(device)
             recon = model(img)
             for i in range(0, len(img)):
                 loss = criterion(recon[i], img[i])
@@ -133,8 +145,8 @@ def complete_run_cae(file_prefix, selected_label=9, p=0.05, train_loss_percent=0
         anomalie = [round(tensor.tolist(),5) for tensor in anomalie]
         return normal, anomalie, anomalie_label
 
-    normal_train, anomalie_train, anomalie_label_train = calculate_losses(train_loader)
-    normal_test, anomalie_test, anomalie_label_test = calculate_losses(test_loader)
+    normal_train, anomalie_train, anomalie_label_train = calculate_losses(train_loader, device)
+    normal_test, anomalie_test, anomalie_label_test = calculate_losses(test_loader, device)
 
     min_loss = min((min(normal_train),min(anomalie_train)))
     max_loss = max((max(normal_train),max(anomalie_train)))
@@ -145,19 +157,23 @@ def complete_run_cae(file_prefix, selected_label=9, p=0.05, train_loss_percent=0
     p = normal_train + anomalie_train
     df = pd.DataFrame({'Loss': p, 
                        'Label': [selected_label]*len(normal_train) + anomalie_label_train}).sort_values(by="Loss")
-    df.to_csv(file_prefix+"train-loss.csv")
+    path = os.path.join(absolute_path, file_prefix+"train-loss.csv")
+    df.to_csv(path)
 
     #t = [0] * len(normal_test) + [1] * len(anomalie_test)
     p = normal_test + anomalie_test
     df = pd.DataFrame({'Loss': p, 
                        'Label': [selected_label]*len(normal_test) + anomalie_label_test}).sort_values(by="Loss")
-    df.to_csv(file_prefix+"test-loss.csv")
+    path = os.path.join(absolute_path, file_prefix+"test-loss.csv")
+    df.to_csv(path)
 
     return
 
 
-if __name__ == "__main__":
-    prefix_num = 1
+if __name__ == "__main__": 
+    pnum = 100
+    #for pnum in range(200, 205):
+    prefix_num = pnum
     num_epochs = 250
     p = 0.25
 
@@ -177,5 +193,9 @@ if __name__ == "__main__":
             if pd.isna(df_train.at[i, str(train_loss_percent)]) or pd.isna(df_test.at[i, str(train_loss_percent)]):
                 print("### " + str(i) + " ### ({})".format(train_loss_percent))
                 file_prefix = "results/{}/".format(prefix_num) + "-".join([str(x).replace(".", ",") for x in [i, p, train_loss_percent, num_epochs]])
+                path = os.path.join(absolute_path, file_prefix+"train-loss.csv")
+                print(path)
+                if os.path.isfile(path):
+                    continue 
                 complete_run_cae(file_prefix=file_prefix, selected_label=i, p=p,train_loss_percent=train_loss_percent, num_epochs=num_epochs)
                 

code/pytorch/models/cae_pytorch.py
@@ -39,7 +39,7 @@ class CAE_pytorch(nn.Module):
         self.dec_conv3 = nn.ConvTranspose2d(in_channels=nf, out_channels=in_channels, kernel_size=3, stride=2, padding=1, output_padding=1)
         self.output_act = nn.Tanh()
 
-    def encode(self, x):
+    """def encode(self, x):
         #print(len(x[0]), len(x[0][0]))
         x = self.enc_act1(self.enc_bn1(self.enc_conv1(x)))
         #print(len(x[0]), len(x[0][0]))
@@ -49,9 +49,21 @@ class CAE_pytorch(nn.Module):
         #print(len(x[0]), len(x[0][0]))
         rep = self.rep_act(self.enc_fc(x.view(x.size(0), -1)))
         #print(len(rep[0]))
+        return rep"""
+        
+    def encode(self, x):
+        #print(len(x[0]), len(x[0][0]))
+        x = self.enc_act1((self.enc_conv1(x)))
+        #print(len(x[0]), len(x[0][0]))
+        x = self.enc_act2((self.enc_conv2(x)))
+        #print(len(x[0]), len(x[0][0]))
+        x = self.enc_act3((self.enc_conv3(x)))
+        #print(len(x[0]), len(x[0][0]))
+        rep = self.rep_act(self.enc_fc(x.view(x.size(0), -1)))
+        #print(len(rep[0]))
         return rep
 
-    def decode(self, rep):
+    """def decode(self, rep):
         #print("Decode")
         #print(len(rep[0]))
         x = self.dec_act0(self.dec_bn0(self.dec_fc(rep)))
@@ -61,6 +73,21 @@ class CAE_pytorch(nn.Module):
         x = self.dec_act1(self.dec_bn1(self.dec_conv1(x)))
         #print(len(x[0]), len(x[0][0]))
         x = self.dec_act2(self.dec_bn2(self.dec_conv2(x)))
+        #print(len(x[0]), len(x[0][0]))
+        x = self.output_act(self.dec_conv3(x))
+        #print(len(x[0]), len(x[0][0]))
+        return x"""
+        
+    def decode(self, rep):
+        #print("Decode")
+        #print(len(rep[0]))
+        x = self.dec_act0((self.dec_fc(rep)))
+        #print(len(x[0]))
+        x = x.view(-1, self.nf * 4, 4, 4)
+        #print(len(x[0]), len(x[0][0]))
+        x = self.dec_act1((self.dec_conv1(x)))
+        #print(len(x[0]), len(x[0][0]))
+        x = self.dec_act2((self.dec_conv2(x)))
         #print(len(x[0]), len(x[0][0]))
         x = self.output_act(self.dec_conv3(x))
         #print(len(x[0]), len(x[0][0]))